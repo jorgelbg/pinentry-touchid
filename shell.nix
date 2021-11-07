965ac7dcda16392e36b4493c3d26ebf9660543f3
Jorge Luis Betancourt <github@jorgelbg.me>
2021-11-07 16:55:44 +0100

    dev: Pin specific versions via nixpkgs
    - `go` is pinned to version go1.16.5

--- shell.nix
+++ shell.nix
@@ -1,4 +1,17 @@
-{ pkgs ? import <nixpkgs> {} }:
+#!/usr/bin/env nix-shell
+#! nix-shell --pure --run "env i_fcolor=red zsh" .
+
+let
+     pkgs = import (builtins.fetchGit {
+         # Descriptive name to make the store path easier to identify
+         name = "my-old-revision";
+         url = "https://github.com/NixOS/nixpkgs/";
+         ref = "refs/heads/nixpkgs-unstable";
+         rev = "860b56be91fb874d48e23a950815969a7b832fbc";
+     }) {};
+
+     goPkg = pkgs.go;
+in with pkgs;
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
