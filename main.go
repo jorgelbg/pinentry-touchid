46a370091f098ff324142356168bf9042ebfb0f5
01apoorv <60395980+01apoorv@users.noreply.github.com>
2023-10-05 16:15:36 -0400

    Changed parameter names

--- main.go
+++ main.go
@@ -70,10 +70,10 @@ const (
 // checkEntryInKeychain executes a search in the current keychain. The search configured to not
 // return the Data stored in the Keychain, as a result this should not require any type of
 // authentication.
-func checkEntryInKeychain(label string) (bool, error) {
+func checkEntryInKeychain(keyInfo string) (bool, error) {
 	query := keychain.NewItem()
 	query.SetSecClass(keychain.SecClassGenericPassword)
-	query.SetAccount(label)
+	query.SetAccount(keyInfo)
 	query.SetMatchLimit(keychain.MatchLimitOne)
 	query.SetReturnData(false)
 	query.SetReturnAttributes(true)
@@ -135,10 +135,10 @@ func WithLogger(logger *log.Logger) KeychainClient {
 }
 
 // passwordFromKeychain retrieves a password given a label from the Keychain
-func passwordFromKeychain(label string) (string, error) {
+func passwordFromKeychain(keyInfo string) (string, error) {
 	query := keychain.NewItem()
 	query.SetSecClass(keychain.SecClassGenericPassword)
-	query.SetAccount(label)
+	query.SetAccount(keyInfo)
 	query.SetMatchLimit(keychain.MatchLimitOne)
 	query.SetReturnData(true)
 
@@ -288,6 +288,8 @@ func GetPIN(authFn AuthFunc, promptFn PromptFunc, logger *log.Logger) GetPinFunc
 			return "", assuanError(fmt.Errorf("invalid keyID: %s", keyID))
 		}
 
+		// s.KeyInfo is always in the form of x/cacheId
+		// https://gist.github.com/mdeguzis/05d1f284f931223624834788da045c65#file-info-pinentry-L357-L362
 		keyInfo := strings.Split(s.KeyInfo, "/")[1]
 
 		keychainLabel := fmt.Sprintf("%s <%s> (%s)", name, email, keyID)
@@ -316,10 +318,6 @@ func GetPIN(authFn AuthFunc, promptFn PromptFunc, logger *log.Logger) GetPinFunc
 				logger.Printf("pinentry-mac didn't return a password")
 				return "", assuanError(fmt.Errorf("pinentry-mac didn't return a password"))
 			}
-
-			// s.KeyInfo is always in the form of x/cacheId
-			// https://gist.github.com/mdeguzis/05d1f284f931223624834788da045c65#file-info-pinentry-L357-L362
-			keyInfo := strings.Split(s.KeyInfo, "/")[1]
 
 			// pinentry-mac can create an item in the keychain, if that was the case, the user will have
 			// to authorize our app to access the item without asking for a password from the user. If
